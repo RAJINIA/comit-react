fef95280b48d6c210bacbfb025f66ae3eb47c372
rajini <rajini1608@gmail.com>
2019-10-15 23:13:39 -0600

    coding

---
+++ lessons/20-js-conversions/index.js
@@ -0,0 +1,11 @@
+let age = 50;
+let firstName = "Rajini";
+let classCompleted = false;
+let finalScore = null;
+
+let winningNumbers;
+
+console.log("Age :", age);
+console.log("First Name :", firstName);
+console.log("Is Course Completed :", classCompleted);
+console.log("Final Score :", finalScore);
