bbc7473859ae2ece0e59265e73afebcfbc56c3f8
rajini <rajini1608@gmail.com>
2019-10-17 15:12:44 -0600

    20-js-conversions

--- lessons/20-js-conversions/index.js
+++ lessons/20-js-conversions/index.js
@@ -9,3 +9,21 @@ console.log("Age :", age);
 console.log("First Name :", firstName);
 console.log("Is Course Completed :", classCompleted);
 console.log("Final Score :", finalScore);
+
+let numToString = String(age);
+console.log("Number to String :", numToString);
+let booleanToString = String(classCompleted);
+console.log("Boolean to String :", booleanToString);
+
+let strToNumber = Number(firstName);
+console.log("String to Number :", strToNumber);
+console.log("String to Number :", Number(finalScore));
+let booleanToNumber = Number(classCompleted);
+console.log("Boolean to Number :", booleanToNumber);
+
+let strToBoolean = Boolean(firstName);
+console.log("String to Boolean :", strToBoolean);
+let numToBoolean = Boolean(age);
+console.log("Number to Boolean :", numToBoolean);
+
+
