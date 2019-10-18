82f8900a5b4134d4cae29be05dd1b3b307c9065c
rajini <rajini1608@gmail.com>
2019-10-17 18:18:52 -0600

    continue

--- lessons/21-js-comparison/index.js
+++ lessons/21-js-comparison/index.js
@@ -1,1 +1,13 @@
 alert("Enter 2 numbers to see if the first is greater than the second");
+
+//let a = 100;
+//let b = 200;
+prompt("Enter first number :");
+//console.log("First number :");
+prompt("Enter second number :");
+//console.log("Second number :");
+
+
+//let result = a > b;
+
+result = confirm("Is first number greater than second?");
