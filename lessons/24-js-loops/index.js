4663ed152c8a8b36b8b3d6926e3679dd350c863f
rajini <rajini1608@gmail.com>
2019-10-18 17:34:29 -0600

    break

--- lessons/24-js-loops/index.js
+++ lessons/24-js-loops/index.js
@@ -7,3 +7,45 @@ for(let a = 0; a < 1000; a++){
     }
     console.log(sum);
 }
+
+// Show numbers from 0 - 10
+// FOR LOOP
+console.log("For loop to show numbers from 0 to 10");
+for (let a = 0; a<= 10; a++){
+    console.log("a = ", a);
+}
+
+// DO LOOP
+console.log("Do loop to show numbers from 0 to 10");
+
+let a = 0;
+do{
+    console.log("a = ", a);
+    a++;
+} while(a <= 10);
+
+//WHILE LOOP
+console.log("While loop to show numbers from 0 to 10");
+
+let b = 0;
+while(b <= 10){
+    console.log("b = ", b);
+    b++;
+}
+
+
+// Show numbers from 100 - 0
+// FOR LOOP
+console.log("For loop to show numbers from 100 to 0");
+for(let a = 100; a >= 0; a--){
+    console.log("a = ", a);
+}
+
+//DO LOOP
+console.log("Do loop to show numbers from 100 to 0");
+let c = 100;
+do(c >= 0){
+    console.log("c = ", c);
+    c-- ;
+}
+
