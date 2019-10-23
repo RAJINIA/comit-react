a96753787a934980fc18c3c80041e2e0f90f29ee
rajini <rajini1608@gmail.com>
2019-10-22 19:21:46 -0600

    break

--- lessons/26-js-functions/index.js
+++ lessons/26-js-functions/index.js
@@ -1,3 +1,4 @@
+// EVEN NUMBER FUNCTION
 function evenNumbers(start, end){
     for (let i = start; i <= end; i++){
         if (i % 2 == 0){
@@ -7,3 +8,13 @@ function evenNumbers(start, end){
 }
 
 evenNumbers(0, 100);
+
+// SHOW USER FUNCTION
+function showUser(){
+    console.log("Name : Rajini");
+    console.log("Age : 30");
+    console.log("Phone Number : 762768732");
+    console.log("Street : Molland Lane");
+    console.log("Postal Code : s7m 1l2");
+    console.log("Are you married : ")
+}
