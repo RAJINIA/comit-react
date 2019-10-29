31bca4db46c90646306be91fa09d27eccff9a21e
rajini <rajini1608@gmail.com>
2019-10-28 22:35:43 -0600

    completed lesson 31

--- lessons/31-js-data-properties/index.js
+++ lessons/31-js-data-properties/index.js
@@ -1,27 +1,20 @@
-//let movieNames = prompt("Enter 3 movies with comma");
-//let arrayMovieNames = movieNames.split(', ');
-//console.log(arrayMovieNames);
+let movieNames = prompt("Enter 3 movies with comma");
+let arrayMovieNames = movieNames.split(', ');
+console.log(arrayMovieNames);
 
-
-let sentence = prompt("Enter a sentence to be reversed");
-// let arraySentence = sentence.split(', ');
-// console.log(arraySentence);
-// let reverseSentence = arraySentence.reverse();
-// console.log(reverseSentence);
-
+//REVERSE STRING
 function reverseString(inputString){
     let str = inputString;
     let arrayStr = inputString.split('');
-    console.log(arrayStr);
+    //console.log(arrayStr);
     let reverseString = arrayStr.reverse();
     //console.log(reverseString);
-    return reverseString;
+    return reverseString.join('');
 }
-
+let sentence = prompt("Enter a sentence to be reversed");
 console.log(reverseString(sentence));
 
 let string = prompt("enter a string");
-
 console.log(reverseString(string));
 
 
